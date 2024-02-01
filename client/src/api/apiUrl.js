ab9d4e51f39b6a9d3970b463338cc35a13d49f6a
Deepak Kumar Chouhan <dk397787@gmail.com>
2024-02-01 06:58:25 +0530

    add deployed url to frontend

--- client/src/api/apiUrl.js
+++ client/src/api/apiUrl.js
@@ -1,5 +1,17 @@
+// Local host URL
 
-export const url = 'http://localhost:5000/api/v2'
+// export const url = 'http://localhost:5000/api/v2'
 
-export const imgUrl = 'http://localhost:5000'
+// export const imgUrl = 'http://localhost:5000'
+
+
+
+// Deployed URL  @https://image-gallery-backend-api-0bvy.onrender.com
+
+export const url = 'https://image-gallery-backend-api-0bvy.onrender.com/api/v2'
+
+export const imgUrl = 'https://image-gallery-backend-api-0bvy.onrender.com'
+
+
+
 
